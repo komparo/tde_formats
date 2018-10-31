ba834b0d92502607e9a0b8a139533c99c71a06a2
Wouter Saelens <wouter.saelens@gmail.com>
2018-10-31 13:49:22 +0100

    add tde_overall format

--- R/datasets.R
+++ R/datasets.R
@@ -1,4 +1,5 @@
 #' Expression matrix
+#' @param ... ...
 #' @export
 gene_expression <- add_validators(
   derived_file,
@@ -7,3 +8,17 @@ gene_expression <- add_validators(
     validate(all(!is.na(expression)), "All values should not be NA")
   }
 )
+
+
+#' Overall trajectory differential expression
+#' @param ... ...
+#' @export
+tde_overall <- add_validators(
+  derived_file,
+  function(design) {
+    tde_overall <- read_csv(h, nrows = 2, header = TRUE, row.names = 1)
+    validate(all(!is.na(expression)), "All values should not be NA")
+  }
+)
+
+
